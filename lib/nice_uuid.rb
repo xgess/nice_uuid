7d5252ab55c4aafb5968c4202986986d968a855e
Arvind Budhram <arvind@shopkeep.com>
2013-11-04 11:24:00 -0500

    Adding a max length for uuid generation

--- lib/nice_uuid.rb
+++ lib/nice_uuid.rb
@@ -1,15 +1,25 @@
 require "nice_uuid/version"
 
 module NiceUuid
+
+  module Errors
+    MaxLengthUuidExceededError = Class.new(StandardError)
+  end
+
   DEFAULT_NUMBER_OF_WORDS = 4
+  MAX_LENGTH_UUID = 40
   DICTIONARY_PATH = File.dirname(__FILE__) + '/length_ordered_sanitized_dictionary.txt'
 
   def self.generate(maximum=36)
-    10000.times do
-      @solution = possible_nice_uuid
-      break if @solution.length <= maximum
+    if maximum <= MAX_LENGTH_UUID
+      10000.times do
+        @solution = possible_nice_uuid
+        break if @solution.length <= maximum
+      end
+      @solution
+    else
+      raise Errors::MaxLengthUuidExceededError.new "Can only generate uuids up to #{MAX_LENGTH_UUID} chars long."
     end
-    @solution
   end
 
   private
--- spec/lib/nice_uuid_spec.rb
+++ spec/lib/nice_uuid_spec.rb
@@ -12,6 +12,27 @@ describe NiceUuid do
       @nice_uuid.count('-').should == (NiceUuid::DEFAULT_NUMBER_OF_WORDS - 1)
     end
 
+    context 'enforces a max uuid length' do
+      it 'raises when trying to create a uuid above the maximum uuid length' do
+        expect{
+          NiceUuid::generate(NiceUuid::MAX_LENGTH_UUID+1)
+        }.to raise_error NiceUuid::Errors::MaxLengthUuidExceededError
+      end
+
+      it 'does not raise when trying to create a uuid up to the maximum uuid length' do
+        expect{
+          NiceUuid::generate(NiceUuid::MAX_LENGTH_UUID)
+        }.not_to raise_error NiceUuid::Errors::MaxLengthUuidExceededError
+      end
+
+      it 'does not raise when trying to create a uuid less than the maximum uuid length' do
+        expect{
+          NiceUuid::generate(NiceUuid::MAX_LENGTH_UUID-1)
+        }.not_to raise_error NiceUuid::Errors::MaxLengthUuidExceededError
+      end
+    end
+
+
     it 'eventually yields a solution for an obnoxious request' do
       NiceUuid::generate(27).length.should <= 27
     end
